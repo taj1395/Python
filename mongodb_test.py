4c3e6440a1bbc54409348f5424c8280f69ad2659
Tajudeen Garuba <tgaruba@my365.bellevue.edu>
2022-07-10 18:29:14 -0500

    init

---
+++ mongodb_test.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.ckrejau.mongodb.net/?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names)
